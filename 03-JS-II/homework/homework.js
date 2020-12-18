9b54795ccddaed4b00216239590ba2eae63d5414
MorganHidden <montielagustin92@gmail.com>
2020-12-18 12:06:16 -0300

    homework sin terminar

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,11 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x > y) {
+    return x;
+  } else {
+   return y;
+  }
 }
 
 function saludo(idioma) {
@@ -14,12 +19,26 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma === "aleman") {
+    return "Guten Tag!";
+  } else if (idioma === "mandarin") {
+    return "Ni Hao!";
+  } else if (idioma === "ingles") {
+    return "Hello!";
+  } else {
+    return "Hola!"
+  }
 }
 
 function esDiezOCinco(numero) {
     // Devuelve "true" si "numero" es 10 o 5
     // De lo contrario, devuelve "false"
     // Tu código:
+    if (numero = 10 || 5) {
+      return true;
+    } else if (numero = !10 || !5) {
+      return false;
+    }
 }
 
 function estaEnRango(numero) {
